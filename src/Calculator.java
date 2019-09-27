38c4d74b844913c69c4c19aec135aa4c75291f42
Shania Hau <shaniahau@gmail.com>
2019-09-27 09:44:50 +0200

    regnemaskine

---
+++ src/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("Skriv dit første tal");
+        int tal1=input.nextInt();
+
+        System.out.println("Skriv dit andet tal");
+        int tal2=input.nextInt();
+
+        System.out.println("Vælg din operator");
+        char operator=input.next().charAt(0);
+        switch(operator){
+            case '+':
+                System.out.println(tal1+tal2);
+                break;
+            case '-':
+                System.out.println(tal1-tal2);
+                break;
+            case '*':
+                System.out.println(tal1*tal2);
+                break;
+            case'/':
+                System.out.println(tal1/tal2);
+                 break;
+        }
+
+    }
+}
